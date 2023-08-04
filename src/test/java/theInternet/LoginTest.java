6ce0fe0da016d53d76461c2744a6395d2ef15fec
Linh-Phan-KatOne <linh.phan@katalon.com>
2023-08-04 10:34:34 +0700

    add todo mvc

--- src/test/java/theInternet/LoginTest.java
+++ src/test/java/theInternet/LoginTest.java
@@ -1,11 +1,14 @@
 package theInternet;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
 
 public class LoginTest {
     /*
@@ -42,5 +45,26 @@ public class LoginTest {
         Assert.assertTrue(driver.findElement(By.id("flash")).isDisplayed());
         driver.quit();
 
+    }
+
+    @Test
+    void createANewTodoSuccessfully(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://todomvc.com/examples/vanillajs/");
+
+        String taskName = "Task 1";
+        driver.findElement(By.cssSelector("input.new-todo")).sendKeys(taskName, Keys.RETURN);
+
+        //Verify the new task display with correct text
+        WebElement taskLabel = driver.findElement(By.xpath("//ul//li//label[.='"+taskName+"']"));
+        Assert.assertTrue(taskLabel.isDisplayed());
+
+        taskName = "Task 2";
+        driver.findElement(By.cssSelector("input.new-todo")).sendKeys(taskName, Keys.RETURN);
+        WebElement lastTodoItem = driver.findElement(By.cssSelector("ul.todo-list>li:last-child"));
+        //Verify the newest task is shown at the end of the list
+        Assert.assertEquals(lastTodoItem.getText(),taskName);
+
+        driver.quit();
     }
 }
